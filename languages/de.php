1696bb2aab8d301c53c579ee8e657c8082516581
Sebastian Daniel <daniel@wobster.de>
2014-03-07 01:34:47 +0100

    Added German translation

---
+++ languages/de.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *
+ * Mobilize German language file
+ * @author s.Daniel
+ *
+ * @package mobilize
+ *
+ */
+
+$german = array(
+
+  'admin:settings:mobilize' => 'Mobilize',
+  'mobilize:panel'      => 'Plugin Optionen',
+
+  'mobilize:copyright'  => 'Copyright &copy; ',
+  'mobilize:elgg'       => 'Powered by Elgg',
+  'mobilize:info:general' => 'Allgemein:',
+  'mobilize:info:message' => 'Startseiten-Willkommensnachricht hinzufügen:',
+  'mobilize:info:teaser'  => 'Benutze Text von?',
+  'mobilize:label:color'  => "Header Farbe, Hex-Farbcode Angaben verwenden - #4690D6",
+  'mobilize:label:fp'   => "Friends picker Javascript deaktivieren?",
+  'mobilize:label:logo'   => "Eigenes Logo anzeigen",
+  'mobilize:label:teaser' => 'Text eingeben:',
+  'mobilize:menu'     => "Untermenü",
+  'mobilize:navigation'   => "Navigation",
+  'mobilize:option:file'  => "Übersetzungsdatei",
+  'mobilize:option:field' => "Text Feld",
+  'mobilize:option:none'  => "Kein",
+  'mobilize:pagetitle'  => 'Das ist Mobilize',
+  'mobilize:teaser'   => 'Willkommen auf unserem Mobile Layout. Bitte melden Sie sich an oder registrieren Sie sich!',
+  'mobilize:tooltip'    => 'Visit Elggzone',
+
+);
+add_translation("de", $german);
+
+?>
